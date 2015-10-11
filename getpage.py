d73d8157a45618f7cdfa4572dc4239a9c49c611c
amar-laksh <zacnethen@gmail.com>
2015-10-12 00:22:42 +0530

    Final stages

--- getpage.py
+++ getpage.py
@@ -3,28 +3,31 @@ import requests
 import cookielib
 
 import Tkinter
+import ttk
 
 class Attendance(Tkinter.Tk):
 
     def __init__(self,parent):
         Tkinter.Tk.__init__(self,parent)
         self.parent = parent
-        self.query = ""
+        self.value_of_combo = 'FYBA'
         self.initialize()
         
 
     def initialize(self):
         self.grid()
+        self.box_value = Tkinter.StringVar()
+        self.box = ttk.Combobox(self.parent, textvariable=self.box_value)
+        self.box['values'] = ('FYBA', 'FYBSC', 'FYBMM','FYBMS', 'FYBSCIT', 'FYTOUR','FYSOFT','SYBA', 'SYBSC', 'SYBMM','SYBMS', 'SYBSCIT', 'SYTOUR','SYSOFT','TYBA', 'TYBSC', 'TYBMMA','TYBMMJ','TYBMS', 'TYBSCIT', 'TYTOUR','TYSOFT')
+        self.box.current(0)
+        self.box.grid(column=0, row=0)
         self.entryVariable = Tkinter.StringVar()
-        self.entryVariable2 = Tkinter.StringVar()
         self.entry = Tkinter.Entry(self,textvariable=self.entryVariable)
-        self.entry2 = Tkinter.Entry(self,textvariable=self.entryVariable2)
-        self.entry.grid(column=0,row=0,sticky='EW')
-        self.entry2.grid(column=0,row=1,sticky='EW')
+        self.entry.grid(column=0,row=1,sticky='EW')
         self.entry.bind("<Return>", self.OnPressEnter)
-        self.entry2.bind("<Return>", self.OnPressEnter)
+        self.box.bind("<<ComboboxSelected>>", self.newselection)
+        self.box.bind("<Return>", self.OnPressEnter)
         self.entryVariable.set(u"Enter rollno here(.eg. 1 ).")
-        self.entryVariable2.set(u"Enter the course here...")
         button = Tkinter.Button(self,text=u"Show Attendance!",command=self.OnButtonClick)
         button.grid(column=1,row=0)
         self.labelVariable = Tkinter.StringVar()
@@ -32,25 +35,28 @@ class Attendance(Tkinter.Tk):
         label.grid(column=0,row=2,columnspan=2,sticky='EW')
         self.labelVariable.set(u"Hello please give a rollno")
 
+    def newselection(self, event):
+        self.value_of_combo = self.box.get()
+        print(self.value_of_combo)
+        
         self.grid_columnconfigure(0,weight=1)
         self.resizable(True,False)
         self.update()
         self.geometry(self.geometry())       
         self.entry.focus_set()
         self.entry.selection_range(0, Tkinter.END)
-        self.entry2.selection_range(0, Tkinter.END)
-
+      
     def OnButtonClick(self):
-        self.labelVariable.set( self.entryVariable.get()+" (You clicked the button)" )
+        self.labelVariable.set( "opening result for roll no: "+self.entryVariable.get()+" (You clicked the button)" )
         self.entry.focus_set()
         self.entry.selection_range(0, Tkinter.END)
-        self.downloadstuff("opening result for roll no: "+self.entryVariable.get()+" in dept: "+self.entryVariable2.get()+" (You pressed ENTER)")
+        self.downloadstuff(self.entryVariable.get(),self.value_of_combo)
         
         
     def OnPressEnter(self,event):
-        self.labelVariable.set( "opening result for roll no: "+self.entryVariable.get()+" in dept: "+self.entryVariable2.get()+" (You pressed ENTER)" )
+        self.labelVariable.set( "opening result for roll no: "+self.entryVariable.get()+" in dept: "+self.value_of_combo+" (You pressed ENTER)" )
         self.entry.focus_set()
-        self.downloadstuff(self.entryVariable.get(),self.entryVariable2.get())
+        self.downloadstuff(self.entryVariable.get(),self.value_of_combo)
 
 
     def getname(self,urls):
@@ -99,11 +105,18 @@ class Attendance(Tkinter.Tk):
 
     def downloadstuff(self,r,dep):
         dept = str(dep)
-        rollno = self.getrollnos(int(r),int(r)+1)
-        s = requests.Session()
-        self.getpage('http://xaviers.edu.in/demouser/Checklogin.php',['myusername', dept, 'mypassword', rollno, 'Submit', 'Login'],s)
-        self.getpage('http://xaviers.edu.in/demouser/StudentAttendance.php',['drpcourse', dept, 'txtrollno', rollno, 'Submit', 'Show Attendance'],s)
-        s.close()
+        if 'TYBMMA' == dept or 'TYBMMJ' == dept:
+            rollno = r
+            s = requests.Session()
+            self.getpage('http://xaviers.edu.in/demouser/Checklogin.php',['myusername', dept, 'mypassword', rollno, 'Submit', 'Login'],s)
+            self.getpage('http://xaviers.edu.in/demouser/StudentAttendance.php',['drpcourse', dept, 'txtrollno', rollno, 'Submit', 'Show Attendance'],s)
+            s.close()
+        else:
+            rollno = self.getrollnos(int(r),int(r)+1)
+            s = requests.Session()
+            self.getpage('http://xaviers.edu.in/demouser/Checklogin.php',['myusername', dept, 'mypassword', rollno, 'Submit', 'Login'],s)
+            self.getpage('http://xaviers.edu.in/demouser/StudentAttendance.php',['drpcourse', dept, 'txtrollno', rollno, 'Submit', 'Show Attendance'],s)
+            s.close()
 
     
     def getrollnos(self,start,end):
